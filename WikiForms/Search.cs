9d560c3e2dc0aecd7e5d69be518436fe99fa4069
Lukas Holmberg <44261196+LHolmberg@users.noreply.github.com>
2020-04-24 12:44:42 +0200

    Change form1 to search

--- WikiForms/Search.cs
+++ WikiForms/Search.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WikiForms
 {
-    public partial class Form1 : Form
+    public partial class Search : Form
     {
-        public Form1()
+        public Search()
         {
             InitializeComponent();
         }
