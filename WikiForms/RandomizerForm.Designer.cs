8ed324b39aa59204a1ed7c459a15e24bc79da844
eduKnightWobbles <44261158+eduKnightWobbles@users.noreply.github.com>
2020-04-24 13:21:56 +0200

    RandomForm added

---
+++ WikiForms/RandomizerForm.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace WikiForms
+{
+    partial class RandomizerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // RandomizerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "RandomizerForm";
+            this.Text = "RandomizerForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
