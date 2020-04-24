e86a5a61a70d92c1ca52733ae8ff1489b9330f54
eduKnightWobbles <44261158+eduKnightWobbles@users.noreply.github.com>
2020-04-24 14:01:50 +0200

    Basic Randomizer Functionality

--- WikiForms/RandomizerForm.Designer.cs
+++ WikiForms/RandomizerForm.Designer.cs
@@ -28,19 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.RandomizerButton = new System.Windows.Forms.Button();
+            this.LinkListBox = new System.Windows.Forms.ListBox();
+            this.TestBox = new System.Windows.Forms.TextBox();
+            this.GoToLinkButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // RandomizerButton
+            // 
+            this.RandomizerButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.RandomizerButton.Location = new System.Drawing.Point(41, 71);
+            this.RandomizerButton.Name = "RandomizerButton";
+            this.RandomizerButton.Size = new System.Drawing.Size(186, 52);
+            this.RandomizerButton.TabIndex = 0;
+            this.RandomizerButton.Text = "Randomize!";
+            this.RandomizerButton.UseVisualStyleBackColor = true;
+            this.RandomizerButton.Click += new System.EventHandler(this.RandomizerButton_Click);
+            // 
+            // LinkListBox
+            // 
+            this.LinkListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.LinkListBox.FormattingEnabled = true;
+            this.LinkListBox.ItemHeight = 31;
+            this.LinkListBox.Location = new System.Drawing.Point(284, 51);
+            this.LinkListBox.Name = "LinkListBox";
+            this.LinkListBox.Size = new System.Drawing.Size(579, 531);
+            this.LinkListBox.TabIndex = 1;
+            this.LinkListBox.SelectedIndexChanged += new System.EventHandler(this.LinkListBox_SelectedIndexChanged);
+            // 
+            // TestBox
+            // 
+            this.TestBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.TestBox.Location = new System.Drawing.Point(934, 304);
+            this.TestBox.Name = "TestBox";
+            this.TestBox.Size = new System.Drawing.Size(152, 38);
+            this.TestBox.TabIndex = 2;
+            // 
+            // GoToLinkButton
+            // 
+            this.GoToLinkButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.GoToLinkButton.Location = new System.Drawing.Point(934, 387);
+            this.GoToLinkButton.Name = "GoToLinkButton";
+            this.GoToLinkButton.Size = new System.Drawing.Size(186, 52);
+            this.GoToLinkButton.TabIndex = 3;
+            this.GoToLinkButton.Text = "Go!";
+            this.GoToLinkButton.UseVisualStyleBackColor = true;
+            this.GoToLinkButton.Click += new System.EventHandler(this.GoToLinkButton_Click);
             // 
             // RandomizerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1179, 629);
+            this.Controls.Add(this.GoToLinkButton);
+            this.Controls.Add(this.TestBox);
+            this.Controls.Add(this.LinkListBox);
+            this.Controls.Add(this.RandomizerButton);
             this.Name = "RandomizerForm";
             this.Text = "RandomizerForm";
+            this.Load += new System.EventHandler(this.RandomizerForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button RandomizerButton;
+        private System.Windows.Forms.ListBox LinkListBox;
+        private System.Windows.Forms.TextBox TestBox;
+        private System.Windows.Forms.Button GoToLinkButton;
     }
 }
--- WikiForms/RandomizerForm.cs
+++ WikiForms/RandomizerForm.cs
@@ -12,9 +12,45 @@ namespace WikiForms
 {
     public partial class RandomizerForm : Form
     {
+        int[] randomizedArray = new int[20];
+        Random randomValue = new Random();
+        int selectedIndex;
+
         public RandomizerForm()
         {
             InitializeComponent();
         }
-    }
+
+        private void RandomizerForm_Load(object sender, EventArgs e)
+        {
+            GoToLinkButton.Enabled = false;
+            TestBox.Enabled = false;
+        }
+
+        private void RandomizerButton_Click(object sender, EventArgs e)
+        {
+            LinkListBox.Items.Clear();
+            TestBox.Clear();
+            GoToLinkButton.Enabled = false;
+
+            for(int i = 0; i < randomizedArray.Length; i++)
+            {
+                randomizedArray[i] = randomValue.Next();
+                LinkListBox.Items.Add(randomizedArray[i]);
+            }
+        }
+
+        private void LinkListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GoToLinkButton.Enabled = true;
+            TestBox.Text = LinkListBox.SelectedIndex.ToString();
+            selectedIndex = LinkListBox.SelectedIndex;
+        }
+
+        private void GoToLinkButton_Click(object sender, EventArgs e)
+        {
+            //Go to link and such
+        }
+
+    }//MAKE THE PROGRAM PAGE SAY "SEARCH" BEFORE PUSHING!!!!!
 }
