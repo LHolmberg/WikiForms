8466e29a0ae70f2e8ea1a24dcda972975d2f1dc6
Lukas Holmberg <44261196+LHolmberg@users.noreply.github.com>
2020-05-08 13:18:58 +0200

    Update DataController.cs

--- WikiForms/DataController.cs
+++ WikiForms/DataController.cs
@@ -15,19 +15,28 @@ namespace WikiForms
 
         public DataController(string searchTerm)
         {
-            JArray result;
-            string URL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm;
+            JArray titles, contents;
+            string titleURL = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm;
+       
 
             using (WebClient wc = new WebClient())
             {
-                var json = wc.DownloadString(URL);
+                var titlesJson = wc.DownloadString(titleURL);
 
-                result = (JArray)JsonConvert.DeserializeObject(json);
+                titles = (JArray)JsonConvert.DeserializeObject(json);
             }
+            
             for (int i = 0; i < result[1].Count(); i++)
             {
                 Dictionary<string, string> value = new Dictionary<string, string>();
-                value.Add(result[1][i].ToString(), result[3][i].ToString());
+                using (WebClient wc = new WebClient())
+                {
+
+                    var contentsJson = wc.DownloadString("https://en.wikipedia.org/w/api.php?action=parse&page=" + titles[1][i] + "&prop=wikitext&formatversion=2");
+
+                    contents = (JArray)JsonConvert.DeserializeObject(json);
+                }
+                value.Add(result[1][i].ToString(), contents[1][i]);
                 list.AddLast(value);
                 Console.WriteLine("Added");
             }
